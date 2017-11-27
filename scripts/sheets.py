a73c86ef78648879cc4d14a01578661f0bd19df1
Armando Aguilar L <a.aguilar@gategeek.com>
2017-11-27 17:23:55 -0600

    Script login auth test in windows server

--- scripts/sheets.py
+++ scripts/sheets.py
@@ -31,22 +31,24 @@ def get_credentials():
         Credentials, the obtained credential.
     """
     home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, 'Documents/httdocs/syncgooglesheetmmsql/')
+    #credential_dir = os.path.join(home_dir, 'Documents/httdocs/syncgooglesheetmmsql/')
+    credential_dir = 'C:\ShellScript2016\syncgooglesheetmmsql\scripts\'
     print(credential_dir)
-    #if not os.path.exists(credential_dir):
-    #    os.makedirs(credential_dir)
-    #credential_path = os.path.join(credential_dir,CLIENT_SECRET_FILE)
-
-    #store = Storage(credential_path)
-    #credentials = store.get()
-    #if not credentials or credentials.invalid:
-        #flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        #flow.user_agent = APPLICATION_NAME
-        #if flags:
-    #        credentials = tools.run_flow(flow, store, flags)
-        #else: # Needed only for compatibility with Python 2.6
-            #credentials = tools.run(flow, store)
-        #print('Storing credentials to ' + credential_path)
+    if not os.path.exists(credential_dir):
+        print('Error: dir dont exists')
+    else :
+        credential_path = os.path.join(credential_dir,CLIENT_SECRET_FILE)
+        print(credential_path)
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        if flags:
+            credentials = tools.run_flow(flow, store, flags)
+        else: # Needed only for compatibility with Python 2.6
+            credentials = tools.run(flow, store)
+        print('Storing credentials to ' + credential_path)
     #return credentials
 
 def main():
