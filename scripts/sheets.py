a2a2138e6997788781dde18d566e0cf96c71905a
Armando Aguilar L <a.aguilar@gategeek.com>
2017-11-27 17:07:08 -0600

    Sheets.py 04 view

--- scripts/sheets.py
+++ scripts/sheets.py
@@ -32,21 +32,23 @@ def get_credentials():
     """
     home_dir = os.path.expanduser('~')
     credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,CLIENT_SECRET_FILE)
+    print (home_dir)
+    print(credential_dir)
+    #if not os.path.exists(credential_dir):
+    #    os.makedirs(credential_dir)
+    #credential_path = os.path.join(credential_dir,CLIENT_SECRET_FILE)
 
-    store = Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        if flags:
-            credentials = tools.run_flow(flow, store, flags)
-        else: # Needed only for compatibility with Python 2.6
-            credentials = tools.run(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
+    #store = Storage(credential_path)
+    #credentials = store.get()
+    #if not credentials or credentials.invalid:
+        #flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        #flow.user_agent = APPLICATION_NAME
+        #if flags:
+    #        credentials = tools.run_flow(flow, store, flags)
+        #else: # Needed only for compatibility with Python 2.6
+            #credentials = tools.run(flow, store)
+        #print('Storing credentials to ' + credential_path)
+    #return credentials
 
 def main():
     """Shows basic usage of the Sheets API.
@@ -56,23 +58,23 @@ def main():
     https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
     """
     credentials = get_credentials()
-    http = credentials.authorize(httplib2.Http())
-    discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?version=v4')
-    service = discovery.build('sheets', 'v4', http=http,discoveryServiceUrl=discoveryUrl)
+    #http = credentials.authorize(httplib2.Http())
+    #discoveryUrl = ('https://sheets.googleapis.com/$discovery/rest?version=v4')
+    #service = discovery.build('sheets', 'v4', http=http,discoveryServiceUrl=discoveryUrl)
 
     #spreadsheetId = '17cWeLtwVbadMM0MZlwXoovif3hJQV4skXMwNlXQwUvo'
     spreadsheetId = '1WBqvSg_D4ugNMgK9s-ykgKw3XAYtiNPTyauskC4784w'
     rangeName = 'Hoja1!A1:F'
-    result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=rangeName).execute()
-    values = result.get('values', [])
+    #result = service.spreadsheets().values().get(spreadsheetId=spreadsheetId, range=rangeName).execute()
+    #values = result.get('values', [])
 
-    if not values:
-        print('No data found.')
-    else:
-        print('Name, Major:')
-        for row in values:
+    #if not values:
+    #    print('No data found.')
+    #else:
+    #    print('Name, Major:')
+    #    for row in values:
             #print columns A and E, which correspond to indices 0 and 4.
-            print('%s,%s,%s,%s,%s,%s' % (row[0],row[1],row[2],row[3],row[4],row[5]))
+    #        print('%s,%s,%s,%s,%s,%s' % (row[0],row[1],row[2],row[3],row[4],row[5]))
 
 
 if __name__ == '__main__':
