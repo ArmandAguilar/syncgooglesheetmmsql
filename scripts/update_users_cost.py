1a238dfab04cbc282877104a25eb5b08eef6fa0f
Armando Aguilar L <a.aguilar@gategeek.com>
2017-12-27 13:52:02 -0600

    I make a update in script , i add try extction in both scripts (create user and update costo ) in the exception notifity by email if there are problmes in the ejection of script

--- scripts/update_users_cost.py
+++ scripts/update_users_cost.py
@@ -3,7 +3,23 @@
 # -*- coding: cp1252 -*-
 # -*- coding: utf-8 -*-
 # -*- coding: IBM850 -*-
+#here section for tokes
+from tokensDB import *
 from credential import *
+from tokensNotification import *
+from notification import *
+#here import connection to DB
+import pypyodbc as pyodbc
+import pymssql
+
+def execute_SQL(sql,DB):
+        valor = 'Procesando..'
+        conn = pymssql.connect(host=hostMSSQL,user=userMSSQL,password=passMSSQL,database=DB)
+        cur = conn.cursor()
+        cur.execute(sql)
+        conn.commit()
+        conn.close()
+        return sql
 
 def update_cost():
     #here code !
@@ -29,8 +45,13 @@ def update_cost():
                 #print ('%s   %s  %s' % (row[0],row[1],row[2]))
                 costo = str(row[20]).replace(',','.')
                 cadena = 'Id :' + str(row[2]) + ' Costo :' + str(costo)
-                sql = 'UPDATE [SAP].[dbo].[RecursosCostos] SET [CostoUnitario] = \'' + str(costo) + '\' WHERE IdRecurso=\'' +  str(row[2]) + '\''
-                print (str(sql))
+                try:
+                    sql = 'UPDATE [SAP].[dbo].[RecursosCostos] SET [CostoUnitario] = \'' + str(costo) + '\' WHERE IdRecurso=\'' +  str(row[2]) + '\''
+                    execute_SQL(sql,'SAP')
+                    print (str(sql))
+                except:
+                    send_notification('Error: MatrizHonorarios 2017','a.aguilar@fortaingenieria.com','Error: se pudo ejecutar la consulsa en el MMSQL Verificar el usuario (ID): ' + str(row[2]))
+
 #################################################################################
 ##                                                                             ##
 ##                              Test Function                                  ##
